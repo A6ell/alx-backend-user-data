ceb212efee8b2ec1e87263a187770efa63ab4db7
ab-ruh <abell.demere@gmail.com>
2024-02-13 20:31:18 +0300

    auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,42 +1,51 @@
 #!/usr/bin/env python3
-"""
-Auth class
-"""
-
-from tabnanny import check
+'''
+module for api authentication
+'''
 from flask import request
-from typing import TypeVar, List
+from typing import List, TypeVar
+
 User = TypeVar('User')
 
 
 class Auth:
     """
-    a class to manage the API authentication
+    class auth
     """
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """
-        returns False - path and excluded_paths
-        """
-        check = path
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        """ Method to require auth """
+        if path is None:
             return True
-        if path[-1] != "/":
-            check += "/"
-        if check in excluded_paths or path in excluded_paths:
-            return False
+
+        if not excluded_paths:
+            return True
+
+        # Add a trailing slash to path for uniformity
+        if not path.endswith('/'):
+            path += '/'
+
+        # Check if path is in excluded_paths
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] == '*':
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            elif path == excluded_path:
+                return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
-        """
-        returns None - request
-        """
+        """ Method to get authorization header """
         if request is None:
             return None
-        return request.headers.get("Authorization")
+
+        # Check if the request contains the 'Authorization' header
+        if 'Authorization' not in request.headers:
+            return None
+
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> User:
-        """
-        returns None - request
-        """
+        """ Method to get current user """
         return None
+
